cfb2cec9611216598072beaa83d4f81b00131ade
Lee Minhoon <hackids@naver.com>
2021-08-26 03:42:24 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Skill from './components/Skill'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      skills: [
+        {
+          category: 'Frontend', techs: [
+            'Java', 'C++'
+          ]
+        },
+        {
+          category: 'Backend', techs: [
+            'Java', 'C++'
+          ]
+        }
+      ],
+      contents: [
+        { title: '제목1', desc: '설명1' },
+        { title: '제목2', desc: '설명2' },
+        { title: '제목3', desc: '설명3' },
+        { title: '제목4', desc: '설명4' }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header></Header>
+        <Skill skills={this.state.skills} ></Skill>
+        <Content contents={this.state.contents}></Content>
+        <Footer></Footer>
+      </div >
+    );
+  }
 }
 
 export default App;
