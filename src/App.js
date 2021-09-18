e8cd14b5e5cd40ebddc117febcb0376514b4e84c
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-09-18 16:45:13 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import { resume } from './resume';
-import Header from './components/header/Container'
-import Summary from './components/summary/Container'
-import Skills from './components/skills/Container'
-import Main from './components/main/Container'
-import Footer from './components/Footer'
-import { Component } from 'react';
+import {
+  headerContent,
+  mainContents,
+  skillsContent,
+  footer,
+  summaryContent,
+} from "./resume";
+import Header from "./components/header/Container";
+import Summary from "./components/summary/Container";
+import Skills from "./components/skills/Container";
+import Main from "./components/main/Container";
+import Footer from "./components/Footer";
+import { Component } from "react";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = resume;
-  }
-
   render() {
     return (
       <div className="App">
-        <Header contents={this.state.header}></Header>
-        <Summary contents={this.state.summary}></Summary>
-        <Skills contents={this.state.skills} ></Skills>
-        <Main contents={this.state.experience}></Main>
-        <Main contents={this.state.projects}></Main>
-        <Main contents={this.state.awards}></Main>
-        <Main contents={this.state.research}></Main>
-        <Main contents={this.state.education} ></Main>
-        <Main contents={this.state.certificate} ></Main>
-        <Footer footer={this.state.footer}></Footer>
-      </div >
+        <Header contents={headerContent} />
+        <Summary contents={summaryContent} />
+        <Skills contents={skillsContent} />
+        {mainContents.map((content) => (
+          <Main contents={content} />
+        ))}
+        <Footer footer={footer} />
+      </div>
     );
   }
 }
