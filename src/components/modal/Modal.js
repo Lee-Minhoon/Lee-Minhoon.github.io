9d7be96d945dbda1fcadcc0cf5f0728780a721e2
Lee Minhoon <hackids@naver.com>
2021-10-05 03:54:15 +0900

    init commit

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import styles from '../../styles.module.scss';
 import modal from './modal.module.scss';
 
 class Modal extends Component {
@@ -19,12 +18,10 @@ class Modal extends Component {
     render() {
         const thumbnails = this.props.images.map((image, index) => {
             if (this.state.index === index) {
-                var currentThumbnail = modal.current;
+                var current = modal.current;
             }
             return (
-                <button className={currentThumbnail} onClick={() => this.imageChange(index)} styles={{ background: "black" }}>
-                    <img src={`projects/${this.props.folder}/${image}.png`} alt='keyword'></img>
-                </button>
+                <img className={current} onClick={() => this.imageChange(index)} src={`projects/${this.props.folder}/${image}.png`} alt='thumbnail'></img>
             )
         });
         const currentImage = this.props.images[this.state.index];
@@ -32,29 +29,27 @@ class Modal extends Component {
 
         return (
             <div className={modal.Modal} onClick={() => this.props.close(null)}>
-                <div className={modal.Wrapper}>
-                    <section onClick={(e) => e.stopPropagation()}>
-                        <header>
-                            <div>{this.props.title}</div>
-                            <svg viewBox="0 0 12 12" onClick={() => this.props.close(null)}>
-                                <polyline points="3 3 6 6 9 3 6 6 9 9 6 6 3 9 6 6"></polyline>
-                                <p>test</p>
-                            </svg>
-                        </header>
-                        <div>
-                            <svg style={{ left: '1%' }} viewBox="0 0 12 18" onClick={() => this.imageChange((this.state.index + length - 1) % length)}>
-                                <polyline points="9 3 3 9 9 15"></polyline>
-                            </svg>
-                            <img src={`projects/${this.props.folder}/${currentImage}.png`}></img>
-                            <svg style={{ right: '1%' }} viewBox="0 0 12 18" onClick={() => this.imageChange((this.state.index + 1) % length)}>
-                                <polyline points="3 3 9 9 3 15"></polyline>
-                            </svg>
-                        </div>
-                        <footer>
-                            {thumbnails}
-                        </footer>
-                    </section>
-                </div>
+                <section onClick={(e) => e.stopPropagation()}>
+                    <header>
+                        <div>{this.props.title}</div>
+                        <svg viewBox="0 0 12 12" onClick={() => this.props.close(null)}>
+                            <polyline points="3 3 6 6 9 3 6 6 9 9 6 6 3 9 6 6"></polyline>
+                            <p>test</p>
+                        </svg>
+                    </header>
+                    <div>
+                        <svg style={{ left: '1%' }} viewBox="0 0 12 18" onClick={() => this.imageChange((this.state.index + length - 1) % length)}>
+                            <polyline points="9 3 3 9 9 15"></polyline>
+                        </svg>
+                        <img src={`projects/${this.props.folder}/${currentImage}.png`}></img>
+                        <svg style={{ right: '1%' }} viewBox="0 0 12 18" onClick={() => this.imageChange((this.state.index + 1) % length)}>
+                            <polyline points="3 3 9 9 3 15"></polyline>
+                        </svg>
+                    </div>
+                    <footer>
+                        {thumbnails}
+                    </footer>
+                </section>
             </ div >
         );
     }
